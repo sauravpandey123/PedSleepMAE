e8aceaafbfa9956cbc68d25cc95df91562b17d30
sauravpandey123 <80170144+sauravpandey123@users.noreply.github.com>
2024-12-27 15:19:26 -0500

    Update 3_1_train_classifier.py

--- scripts/3_1_train_classifier.py
+++ scripts/3_1_train_classifier.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
                         help='Directory to save training logs.')
     
     parser.add_argument('--dataset_dir', type=str, default='hdf_batches',
-                        help='Path to the dataset directory.')
+                        help='Path to the combined HDF5 data files.')
 
     parser.add_argument('--emb_dim', type=int, default=64,
                         help='Embedding dimension for the transformer.')
